4ea08b3acc277c18e9a8ee650f5218dc8c617433
0xBono <iam@Sonjim.in>
2020-08-09 21:02:11 +0900

    chore: remove env...

--- src/ApolloClient.ts
+++ src/ApolloClient.ts
@@ -1,8 +1,8 @@
 import ApolloClient from "apollo-boost";
-import { API_HOST } from 'react-native-dotenv'
+// import {API_HOST} from 'react-native-dotenv'
 
 const client = new ApolloClient({
-    uri: process.env.API_HOST
+    uri: 'http://localhost:7777/graphql'
 });
 
 export default client;
